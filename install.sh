a041640018f7089e2ba5a36aed39a9329f5bbd8b
Nick Harshfield <NHarshfield@techshot.com>
2017-10-02 18:12:48 -0400

    Add installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+#ln -s -f .bashrc ~/.bashrc
+#ln -s -f .bash_aliases ~/.bash_aliases
+#ln -s -f .vimrc ~/.vimrc
+#ln -s -f .mvprc ~/.mvprc
+
+cp .bashrc ~/.bashrc
+cp .bash_aliases ~/.bash_aliases
+cp .vimrc ~/.vimrc
+cp .mvprc ~/.mvprc
+
